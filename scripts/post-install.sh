ac986590e061d9e1f92b1331ab259c9f10025bb9
Chris Martin <chris.martin@fourkitchens.com>
2016-06-13 03:00:27 -0500

    Add post-install.sh and update pre-install.sh to not create failures the first run

--- scripts/post-install.sh
+++ scripts/post-install.sh
@@ -1,3 +1,10 @@
+
+# Destroy current symlinks.
+for file in ~/{.gitconfig,.profile,.bash_profile,.bashrc,.eslintrc,.gitignore_global,composer.json}; do
+  test -h $file && rm $file || echo "$file is not symlink";
+done
+unset file
+
 # Create symbolic links in your home directory
 ln -s ~/repos/setup.environment/SAVE_ME_DONT_DELETE/.gitconfig ~/.gitconfig
 ln -s ~/repos/setup.environment/symlinks/.bash_profile ~/.bash_profile
